cdf33bbae74ff06287235148f15892ce8b9fed76
Əli Hüseynov <76520177+elihuseynov@users.noreply.github.com>
2021-04-14 12:36:38 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
